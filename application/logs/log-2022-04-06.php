297ae6abd6f6048327c6211d116707a2c1499085
Rajeev Raghavan <rajeev7000raghavan@gmail.com>
2022-04-06 11:00:11 +0530

    product controller error fixed

--- application/logs/log-2022-04-06.php
+++ application/logs/log-2022-04-06.php
@@ -10,3 +10,11 @@ ERROR - 2022-04-06 09:20:07 --> Severity: Notice --> Undefined variable: notific
 ERROR - 2022-04-06 09:20:07 --> Severity: Warning --> Invalid argument supplied for foreach() /home2/woztijwy/public_html/VENAD/application/views/template/header.php 149
 ERROR - 2022-04-06 09:20:12 --> Severity: Notice --> Undefined variable: notifications /home2/woztijwy/public_html/VENAD/application/views/template/header.php 149
 ERROR - 2022-04-06 09:20:12 --> Severity: Warning --> Invalid argument supplied for foreach() /home2/woztijwy/public_html/VENAD/application/views/template/header.php 149
+ERROR - 2022-04-06 10:52:10 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'woztijwy_gentle'@'localhost' (using password: YES) C:\wamp64\www\VENAD\system\database\drivers\mysqli\mysqli_driver.php 401
+ERROR - 2022-04-06 10:52:10 --> Unable to connect to the database
+ERROR - 2022-04-06 10:56:05 --> 404 Page Not Found: Assets/js
+ERROR - 2022-04-06 10:56:22 --> Severity: Compile Error --> Cannot redeclare Product::get() C:\wamp64\www\VENAD\application\controllers\Product.php 219
+ERROR - 2022-04-06 10:57:35 --> Severity: Notice --> Undefined variable: notifications C:\wamp64\www\VENAD\application\views\template\header.php 297
+ERROR - 2022-04-06 10:57:35 --> Severity: Warning --> Invalid argument supplied for foreach() C:\wamp64\www\VENAD\application\views\template\header.php 297
+ERROR - 2022-04-06 10:57:41 --> Severity: Notice --> Undefined variable: notifications C:\wamp64\www\VENAD\application\views\template\header.php 297
+ERROR - 2022-04-06 10:57:41 --> Severity: Warning --> Invalid argument supplied for foreach() C:\wamp64\www\VENAD\application\views\template\header.php 297
